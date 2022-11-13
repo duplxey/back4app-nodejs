8a5bdbc42d701fb336df536288892b7b3774599f
Nik Tomazic <nick.tomazic@gmail.com>
2022-11-13 14:58:54 +0100

    added README.md, code formatting, updated base.twig with URLs

---
+++ README.md
@@ -0,0 +1,5 @@
+# nodejs-back4app
+
+This project is used as a demonstration on how to deploy a [Node.js](https://nodejs.org/en/) application to [Back4app](https://www.back4app.com/).
+
+For more information read the [article](#).
--- cloud/app.js
+++ cloud/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'twig');
+app.set("views", __dirname + "/views");
+app.set("view engine", "twig");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-require('./routes');
+require("./routes");
